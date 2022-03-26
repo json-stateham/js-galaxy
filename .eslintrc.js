971790265c30461a33e1e4e4dcbce9e4f69ca6a9
json_stateham <roman.pogo@yandex.ru>
2022-03-26 23:52:35 +0600

    removed plugins with nested deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,8 @@ module.exports = {
     es2021: true,
     node: true
   },
-  plugins: ['jest', 'jest-dom', 'node', 'prettier', 'promise', 'regexp', 'security'],
-  extends: [
-    'eslint:recommended',
-    'plugin:jest/recommended',
-    'plugin:jest-dom/recommended',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-    'plugin:promise/recommended',
-    'plugin:regexp/recommended',
-    'plugin:security/recommended'
-  ],
+  plugins: ['jest'],
+  extends: ['eslint:recommended', 'plugin:jest/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
